0fe04392015a11c815b82337839ec7af745ae8da
saimom_costa@hotmail.com <saimom_costa@hotmail.com>
2020-04-08 11:47:56 -0300

    Detalhes do Plano

--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use App\Models\Plan;
 use Illuminate\Http\Request;
@@ -21,5 +22,28 @@ class PlanController extends Controller
         $plans = $this->repository->latest()->paginate();
 
         return view('admin.pages.plans.index',compact('plans'));
+    }
+
+    public function create()
+    {
+        return view('admin.pages.plans.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['url'] = Str::kebab($request->name);
+        $this->repository->create($data);
+
+        return redirect()->route('plans.index');
+    }
+
+    public function show($url)
+    {
+        $plan = $this->repository->where('url', $url)->first();
+
+        if(!$plan)
+            return redirect()->back();
+        return view('admin.pages.plans.show',compact('plan'));
     }
 }
