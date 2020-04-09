451227b90a5ab57b280e286f4ee8d79d047e5ed8
saimom_costa@hotmail.com <saimom_costa@hotmail.com>
2020-04-09 11:03:29 -0300

    Commit First

--- resources/views/admin/pages/plans/details/index.blade.php
+++ resources/views/admin/pages/plans/details/index.blade.php
@@ -1,43 +1,34 @@
 @extends('adminlte::page')
 
-@section('title', 'Planos')
+@section('title', "Detalhes do Plano {$plan->name}")
 
 @section('content_header')
 
     <ol class="breadcrumb">
         <li><a class="breadcrumb-item" href="{{ route('admin.index') }}">Dashboard</a></li>
-        <li><a class="breadcrumb-item active" href="{{ route('plans.index') }}" class=""> / Planos</a></li>
+        <li><a class="breadcrumb-item" href="{{ route('plans.index') }}" class=""> / Planos</a></li>
+        <li><a class="breadcrumb-item" href="{{ route('plans.show', $plan->url) }}" class=""> / {{ $plan->name }}</a></li>
+        <li><a class="breadcrumb-item active" href="{{ route('details.plan.index', $plan->url) }}" class="active"> / Detalhes</a></li>
     </ol>
 
-    <h1>Planos <a href="{{ route('plans.create') }}" class="btn btn-dark">ADD</a></h1>
+    <h1>Detalhes do Plano {{ $plan->name }} <a href="{{ route('plans.create') }}" class="btn btn-dark">ADD</a></h1>
 @stop
 
 @section('content')
     <div class="card">
-        <div class="card-header">
-            <form action="{{ route('plans.search') }}" class="POST" class="form-inline">
-                @csrf
-                <input type="text" name="filter" placeholder="Nome" class="form-control" value="{{ $filters['filter'] ?? '' }}">
-                <br>
-                <button type="submit" class="btn btn-dark mb-2">Filtrar</button>
-            </form>
-        </div>
         <div class="card-body">
             <table class="table table-condensed">
                 <thead>
                     <tr>
                         <th>Nome</th>
-                        <th>Preço</th>
                         <th width='250'>Ações</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($plans as $plan)
+                    @foreach($details as $detail)
                     <tr>
-                        <td>{{ $plan->name }}</td>
-                        <td>R$ {{ number_format($plan->price, 2, ',', '.') }}</td>
+                        <td>{{ $detail->name }}</td>
                         <td width='10px;'>
-                            <a href="{{ route('details.plan.index', $plan->url) }}" class="btn btn-primary">Detalhes</a>
                             <a href="{{ route('plans.edit', $plan->url) }}" class="btn btn-info">Editar</a>
                             <a href="{{ route('plans.show', $plan->url) }}" class="btn btn-warning">Ver</a>
                         </td>
@@ -48,9 +39,9 @@
         </div>
         <div class="card-footer">
             @if(isset($filters))
-            {!! $plans->appends($filters)->links() !!}
+            {!! $details->appends($filters)->links() !!}
             @else
-            {!! $plans->links() !!}
+            {!! $details->links() !!}
             @endif
         </div>
     </div>
