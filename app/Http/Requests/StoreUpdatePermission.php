ec24c9a1d5e0c606f3be0b7e6db463e00b57610d
saimom_costa@hotmail.com <saimom_costa@hotmail.com>
2020-04-09 19:57:25 -0300

    Novo Módulo Permissions vs Profile

---
+++ app/Http/Requests/StoreUpdatePermission.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUpdatePermission extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        //Ele pega a posição na URL
+        //http://larafood.test/admin(1)/profiles(2)/plano-update2(3)/edit(4)
+        $id = $this->segment(3);
+
+        return [
+            'name' => "required|min:3|max:255|unique:profiles,name,{$id},id",
+            'description' => 'nullable|min:3|max:255',
+        ];
+    }
+}
---
+++ resources/views/admin/pages/permissions/index.blade.php
@@ -0,0 +1,55 @@
+@extends('adminlte::page')
+
+@section('title', 'Permissões')
+
+@section('content_header')
+
+    <ol class="breadcrumb">
+        <li><a class="breadcrumb-item" href="{{ route('admin.index') }}">Dashboard</a></li>
+        <li><a class="breadcrumb-item active" href="{{ route('permissions.index') }}" class=""> / Permissões</a></li>
+    </ol>
+
+    <h1>Permissões <a href="{{ route('permissions.create') }}" class="btn btn-dark">ADD</a></h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <form action="{{ route('permissions.search') }}" class="POST" class="form-inline">
+                @csrf
+                <input type="text" name="filter" placeholder="Filtro" class="form-control" value="{{ $filters['filter'] ?? '' }}">
+                <br>
+                <button type="submit" class="btn btn-dark mb-2">Filtrar</button>
+            </form>
+        </div>
+        <div class="card-body">
+            <table class="table table-condensed">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th width='250'>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($permissions as $permission)
+                    <tr>
+                        <td>{{ $permission->name }}</td>
+                        <td width='10px;'>
+                            {{-- <a href="{{ route('details.permission.index', $permission->url) }}" class="btn btn-primary">Detalhes</a> --}}
+                            <a href="{{ route('permissions.edit', $permission->id) }}" class="btn btn-info">Editar</a>
+                            <a href="{{ route('permissions.show', $permission->id) }}" class="btn btn-warning">Ver</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            @if(isset($filters))
+            {!! $permissions->appends($filters)->links() !!}
+            @else
+            {!! $permissions->links() !!}
+            @endif
+        </div>
+    </div>
+@stop
